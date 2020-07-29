1212cd6807e5ea0e8a111426ffc407c253d1b185
Michael Miles <michael@miles-me.net>
2020-07-29 17:54:13 -0500

    Add Strings and escape char

---
+++ Section3/EscapeChar.py
@@ -0,0 +1,27 @@
+__author__ = "Michael E Miles"
+
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner sain \"No, no, 'e's uh,...he's resting\".")
+
+print("""THe pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSplitString = """This string has been
+split over
+several 
+lines"""
+print(anotherSplitString)
+
+# escape the end of line eol
+anotherSplitString2 = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitString2)
--- Section3/Strings.py
+++ Section3/Strings.py
@@ -1,2 +1,12 @@
 __author__ = "Michael E Miles"
 
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We cna even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name: ")
+
+# if we want a space we can add that too
+print(greeting + ' ' + name)
