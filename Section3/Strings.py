2e02003bc05bc7655513daabcbd20796f823fbbb
Michael Miles <michael@miles-me.net>
2020-08-04 16:35:18 -0500

    Update Strings.py
    String types analyzed.

--- Section3/Strings.py
+++ Section3/Strings.py
@@ -6,7 +6,20 @@ print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name: ")
+name = "Michael"
+print(greeting + name)
 
 # if we want a space we can add that too
 print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+ageInWords = "2 years"
+print(age)
+print(type(age))
+
+print(name + " is " + age + " years old")
