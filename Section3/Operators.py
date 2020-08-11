1156389c72b0b83d57656b88215846fec2366b08
Michael Miles <michael@miles-me.net>
2020-08-11 14:25:36 -0500

    Continue with Operations
    Order of operations and expressions
    Create Strings2.py file

--- Section3/Operators.py
+++ Section3/Operators.py
@@ -11,7 +11,14 @@ print(a % b)    # 0 modulo: the remainder after integer division
 
 print()
 
-for i in range(1, a // b):  # Using integer division will work as if using/
-    # the number 4 but using regular division / would result in a float and/
-    # produce an error
-    print(i)
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)
+print(((a + b) / 3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print(a / (b * a) / b)
