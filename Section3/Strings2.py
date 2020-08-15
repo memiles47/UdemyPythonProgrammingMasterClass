e9f0114418b5b196808eb8f9a1e7a519565245b0
Michael Miles <michael@miles-me.net>
2020-08-15 18:05:13 -0500

    Update Strings2.py

--- Section3/Strings2.py
+++ Section3/Strings2.py
@@ -17,8 +17,13 @@ print()
 print(parrot[0:6])  # Norweg
 print(parrot[-14:-8])
 
-print(parrot[-4:-2])
-print(parrot[-4:12])
+print(parrot[0:6:2])  # Nre
+print(parrot[:6:3])  # Nw
 
-print(parrot[-4:])
-print(parrot[-13:-11])
+number = "9,223;372:036 854,775;807"
+separators = number[1::4]
+print(separators)
+
+values = "".join(char if char not in separators
+                 else " " for char in number).split()
+print([int(val) for val in values])
