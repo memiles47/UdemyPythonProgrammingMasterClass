ce714c3bf448baea14d63479a9c6323143678a5a
Michael Miles <michael@miles-me.net>
2020-08-16 08:07:18 -0500

    Update Strings2.py

--- Section3/Strings2.py
+++ Section3/Strings2.py
@@ -24,6 +24,7 @@ number = "9,223;372:036 854,775;807"
 separators = number[1::4]
 print(separators)
 
+# Example and not part of this lesson
 values = "".join(char if char not in separators
                  else " " for char in number).split()
 print([int(val) for val in values])
