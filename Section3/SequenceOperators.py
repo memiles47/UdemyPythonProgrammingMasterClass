686a2dcd94cf13802fa9ea1644188b7ff034801d
Michael Miles <michael@miles-me.net>
2020-08-19 04:31:49 -0500

    Create SequenceOperators.py

---
+++ Section3/SequenceOperators.py
@@ -0,0 +1,20 @@
+__author__ = "Michael E Miles"
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords"
+
+print(string1 + string2 + string3 + string4 + string5)
+print("he's " "probably " "pining " "for the " "fjords")
+
+print("Hello " * 5)
+
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+
+today = "friday"
+print("day" in today)  # s/b True
+print("fri" in today)  # s/e True
+print("thru" in today)  # s/b False
+print("parrot" in "fjord")  # s/b False
