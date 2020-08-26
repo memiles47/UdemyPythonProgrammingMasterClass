20fb3deea2512c283b70b38cbf5e6d21203ab861
Michael Miles <michael@miles-me.net>
2020-08-26 07:20:47 -0500

    Update Formatting.py

--- Section3/Formatting.py
+++ Section3/Formatting.py
@@ -21,5 +21,7 @@ print("Pi is approximately {0:62.50f}".format(22 / 7))
 print("Pi is approximately {0:<72.50f}".format(22 / 7))
 print("Pi is approximately {0:<72.54f}".format(22 / 7))
 
+print()
+
 for i in range(1, 13):
     print("No. {:2} squared is {:3} and cubed is {:4}".format(i, i ** 2, i ** 3))
