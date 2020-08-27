c809b537798b888590ea8eb5197b9ba623a1b29d
Michael Miles <michael@miles-me.net>
2020-08-27 05:47:12 -0500

    Update Strings.py

--- Section3/Strings.py
+++ Section3/Strings.py
@@ -24,4 +24,8 @@ print(type(age))
 
 # If you were to bind a string to age you would avoid an error in\
 # the next statement
-print(name + " is " + age + " years old")
+print(name + f" is {age} years old")
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
