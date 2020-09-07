a8b2f88b6daa0c0c5943647239607ff62b595712
Michael Miles <michael@miles-me.net>
2020-09-07 08:33:50 -0500

    Update GuessingGame.py

--- Section4/GuessingGame.py
+++ Section4/GuessingGame.py
@@ -3,19 +3,51 @@ answer = 5  # Starting guess
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Pleas guess higher")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry you have not guessed correctly")
-elif guess > answer:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry you have not guessed correctly")
+# region Second Iteration
+# if guess != answer:
+#     if guess < answer:
+#         print("Please guess higher")
+#     else:  # guess must be greater than answer
+#         print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it first time")
+# endregion
+
+# region First Iteration
+# if guess < answer:
+#     print("Pleas guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry you have not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry you have not guessed correctly")
+# else:
+#     print("You got it the first time")
+# endregion
+
+# region Challenge
+if guess == answer:
+    print("You got it first time")
 else:
-    print("You got it the first time")
+    if guess < answer:
+        print("Please guess higher")
+    else:  # guess must be greater than answer
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+# endregion
