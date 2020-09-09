200d126d88a8b0a609545d895351031db981df63
Michael Miles <michael@miles-me.net>
2020-09-09 07:30:31 -0500

    Update Conditions.py

--- Section4/Conditions.py
+++ Section4/Conditions.py
@@ -1,1 +1,5 @@
 __author__ = "Michael E Miles"
+age = int(input("How old are you? "))
+# if age >= 16 and age <= 65:
+if 16 <= age <= 65:  # Simplified if statement
+    print("Have a good day at work")
