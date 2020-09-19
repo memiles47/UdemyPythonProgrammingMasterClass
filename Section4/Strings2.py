b120c0ce96656b75044f51231349d5f9ae2724b7
Michael Miles <michael@miles-me.net>
2020-09-19 18:51:42 -0500

    Create Strings2.py

---
+++ Section4/Strings2.py
@@ -0,0 +1,12 @@
+__author__ = "Michael E Miles"
+number = "9,223;372:036 854,775;807"
+# separators = number[1::4]
+separators = ""
+
+for char in number:
+    if not char.isnumeric():
+        separators = separators + char
+
+print(separators)
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val) for val in values])
