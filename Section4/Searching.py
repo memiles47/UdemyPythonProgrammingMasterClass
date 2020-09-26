84f3afa6e6c09751525c5cc2f44c1eb2959e118c
Michael Miles <michael@miles-me.net>
2020-09-26 09:12:46 -0500

    Update Searching.py

--- Section4/Searching.py
+++ Section4/Searching.py
@@ -1,1 +1,12 @@
 __author__ = "Michael E Miles"
+shoppingList = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+itemToFind = "spam"
+foundAt = None
+
+# for index in range(6)
+for index in range(len(shoppingList)):
+    if shoppingList[index] == itemToFind:
+        foundAt = index
+        break
+
+print("Item found at postion {}".format(foundAt))
