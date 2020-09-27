f552317537b2283de8c837df4e68e03c74f2c087
Michael Miles <michael@miles-me.net>
2020-09-27 14:16:50 -0500

    Update Searching.py

--- Section4/Searching.py
+++ Section4/Searching.py
@@ -1,12 +1,19 @@
 __author__ = "Michael E Miles"
 shoppingList = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 itemToFind = "spam"
+# itemToFind = "albatross"
 foundAt = None
 
 # for index in range(6)
-for index in range(len(shoppingList)):
-    if shoppingList[index] == itemToFind:
-        foundAt = index
-        break
+# for index in range(len(shoppingList)):
+#     if shoppingList[index] == itemToFind:
+#         foundAt = index
+#         break
 
-print("Item found at postion {}".format(foundAt))
+if itemToFind in shoppingList:
+    foundAt = shoppingList.index(itemToFind)
+
+if foundAt is not None:
+    print("Item {} found at position {}".format(itemToFind, foundAt))
+else:
+    print("{} not found".format(itemToFind))
