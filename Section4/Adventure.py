09743950cc5ec46643cf25c242319827fd4fa4d2
Michael Miles <michael@miles-me.net>
2020-09-29 09:05:16 -0500

    While Loops

--- Section4/Adventure.py
+++ Section4/Adventure.py
@@ -3,5 +3,8 @@ availableExits = ["north", "south", "east", "west"]
 chosenExit = ""
 while chosenExit not in availableExits:
     chosenExit = input("Please choose a direction: ")
+    if chosenExit.casefold() == "quit":
+        print("Game over")
+        break
 
 print("aren't you glad you got out of there")
--- Section4/WhileLoop.py
+++ Section4/WhileLoop.py
@@ -4,4 +4,4 @@ __author__ = "Michael E Miles"
 i = 0
 while i < 10:
     print("i is now {}".format(i))
-i += 1
+    i += 1
