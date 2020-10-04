c82af664ffe46d4fccbe402eaa305b4bb22b9104
Michael Miles <michael@miles-me.net>
2020-10-04 06:59:15 -0500

    Update HiLo.py

--- Section4/HiLo.py
+++ Section4/HiLo.py
@@ -4,22 +4,24 @@ high = 1000
 print("please think of a number between {} and {}".format(low, high))
 input("Press <ENTER> to start")
 
-guess = 1
+guesses = 1
 while True:
+    print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2
     highLow = input("My guess is {}. Should I guess higher or lower? "
-                    "Enter h or l, or c if my guess was correct"
+                    "Enter h or l, or c if my guess was correct: "
                     .format(guess)).casefold()
 
     if highLow == "h":
-        print()  # Guess higher. The low end of the range becomes 1 greater than the guess.
-        # An if statement must be followed by an indented statement not a comment
-
+        # Guess higher. The low end of the range becomes 1 greater than the guess.
+        low = guess + 1
     elif highLow == "l":
-        print()  # Guess lower. The high end of the range becomes one less than the guess.
-
+        # Guess lower. The high end of the range becomes one less than the guess.
+        high = guess - 1
     elif highLow == "c":
-        print("I got it in {} guesses!".format(guess))
-
+        print("I got it in {} guesses!".format(guesses))
+        break
     else:
-        print("Please enter h, l or c")
+        print("Please enter h, l or c: ")
+
+    guesses += 1
