355edc6840bf5186483268a0ac4a45be147b0ae9
Michael Miles <michael@miles-me.net>
2020-10-12 16:18:46 -0500

    Update HiLo.py

--- Section4/HiLo.py
+++ Section4/HiLo.py
@@ -5,8 +5,8 @@ print("please think of a number between {} and {}".format(low, high))
 input("Press <ENTER> to start")
 
 guesses = 1
-while True:
-    print("\tGuessing in the range of {} to {}".format(low, high))
+while low != high:
+    # print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2
     highLow = input("My guess is {}. Should I guess higher or lower? "
                     "Enter h or l, or c if my guess was correct: "
@@ -25,3 +25,7 @@ while True:
         print("Please enter h, l or c: ")
 
     guesses += 1
+
+else:
+    print("You thought of the number {}".format(low))
+    print("I got it in {} guesses".format(guesses))
