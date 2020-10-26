6889a04515e1381ca3e9cb056f2bae3cd403fe79
Michael Miles <michael@miles-me.net>
2020-10-26 18:04:54 -0500

    Update SectionFourChallenge.py

--- Section4/SectionFourChallenge.py
+++ Section4/SectionFourChallenge.py
@@ -1,4 +1,37 @@
 __author__ = "Michael E Miles"
 quote = """
-instructions for section four challenge
+Write a program to print a number of options and allow the user to
+select an option from the list.
+
+The options should be numbered from 1 to 9 - although you can use less
+than 9 options if you want.
+
+Make sure there are at least 4 options.
+
+The program should continue looping allowing the user to choose one of
+the options each time around.
+
+The loops should only terminate when the user chooses 0.
+
+If the user makes a valid choice the program should print a short message
+and the message should include the value that they typed.
+
+Don't print a different message for each choice - the only thing that
+should change is the number they typed.
+
+Although you could print the description I want show you a much better
+way of doing that later.
+
+We're keeping this simple because there are still lots of things we haven't
+covered yet.
+
+If their choice isn't one of the options in the menu, nothing should be
+printed although you will see their input on the screen.
+
+As an optional extra, modify the program so that the menu is printed 
+again if they choose an invalid option.
+
+Be careful with that one: you may start off by duplicating the code to
+print the menu but it's possible to write the program without duplicating
+the print lines.
 """
