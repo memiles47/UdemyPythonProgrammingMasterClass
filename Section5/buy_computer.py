82295d5e2472cc4738d862456257f7214fc250ae
Michael Miles <michael@miles-me.net>
2020-10-28 09:18:37 -0500

    Working with lists

---
+++ Section5/buy_computer.py
@@ -0,0 +1,32 @@
+__author__ = "Michael E Miles"
+current_choice = "-"
+computer_parts = []  # Create an empty list
+
+while current_choice != "0":
+    if current_choice in "123456":
+        print("Adding {}".format(current_choice))
+        if current_choice == "1":
+            computer_parts.append("computer")
+        elif current_choice == "2":
+            computer_parts.append("monitor")
+        elif current_choice == "3":
+            computer_parts.append("keyboard")
+        elif current_choice == "4":
+            computer_parts.append("mouse")
+        elif current_choice == "5":
+            computer_parts.append("mouse mat")
+        elif current_choice == "6":
+            computer_parts.append("HDMI cable")
+    else:
+        print("Please add options from the list below:")
+        print("1: computer")
+        print("2: monitor")
+        print("3: keyboard")
+        print("4: mouse")
+        print("5: mouse mat")
+        print("6: HDMI cable")
+        print("0: to finish")
+
+    current_choice = input()
+
+print(computer_parts)
---
+++ Section5/number_lists.py
@@ -0,0 +1,15 @@
+__author__ = "Michael E Miles"
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
+
+print()
+print(len(even))
+print(len(odd))
+
+print()
+print("mississippi".count("issi"))
