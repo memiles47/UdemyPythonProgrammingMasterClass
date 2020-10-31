e814c7ce2bf6c63de4390cfa8119ec5f8ce1edca
Michael Miles <michael@miles-me.net>
2020-10-31 05:36:11 -0500

    Update lists_intro.py

--- Section5/lists_intro.py
+++ Section5/lists_intro.py
@@ -5,13 +5,3 @@ computer_parts = ["computer",
                   "mouse",
                   "mouse mat"
                   ]
-
-for part in computer_parts:
-    print(part)
-
-print()
-print(computer_parts[2])
-
-print()
-print(computer_parts[0:3])
-print(computer_parts[-1])
