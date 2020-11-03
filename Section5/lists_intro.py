efac5e0df15bcdafc7c1f715d8b4d896f828be8f
Michael Miles <michael@miles-me.net>
2020-11-03 06:53:45 -0600

    Update lists_intro.py

--- Section5/lists_intro.py
+++ Section5/lists_intro.py
@@ -7,5 +7,8 @@ computer_parts = ["computer",
                   ]
 print(computer_parts)
 
-computer_parts[3] = "trackball"
+# computer_parts[3] = "trackball"
+print(computer_parts[3:])
+
+computer_parts[3:] = ["trackball"]
 print(computer_parts)
