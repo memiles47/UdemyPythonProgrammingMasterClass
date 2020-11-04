3e031b2d2dbfc99701147364df36edab30913d30
Michael Miles <michael@miles-me.net>
2020-11-04 09:47:13 -0600

    Update tuples_intro.py

--- Section5/tuples_intro.py
+++ Section5/tuples_intro.py
@@ -1,17 +1,13 @@
 __author__ = "Michael E Miles"
-welcome = ("Welcome to my Nightmare", "Alice Cooper", 1975,)
-bad = ("Bad Company", "Bad Company", 1974,)
-budgie = ("Nightflight", "Budgie", 1981,)
-imelda = ("More Mayhem", "Emilda May", 2011,)
-metallica = ("Ride the Lightning", "Metallica", 1984,)
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+print(len(albums))
 
-metallica2 = list(metallica)
-print(metallica2)
-
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+for name, artist, year in albums:
+    print("album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
