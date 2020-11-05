221203ab9d99d944f5be064e1eb081aa10afa855
Michael Miles <michael@miles-me.net>
2020-11-05 08:12:23 -0600

    Update functions.py
    Complete mini  challenge with success however did not think to call one function from the new function.

--- Section6/functions.py
+++ Section6/functions.py
@@ -9,7 +9,16 @@ def multiply(x, y):
 def is_palindrome(string):
     # backwards = string[::-1]
     # return backwards == string
-    return string[::-1] == string
+    return string[::-1].casefold() == string.casefold()
+
+
+def palindrome_sentence(string):
+    check_string = ""
+    for char in string:
+        if char.isalnum():
+            check_string += char
+
+    return is_palindrome(check_string)
 
 
 answer = multiply(10.5, 4)
@@ -25,7 +34,13 @@ for val in range(1, 5):
     print(two_times)
 
 word = input("Please enter a work to check: ")
-if is_palindrome(word.casefold()):
+if is_palindrome(word):
     print("'{}' is a palindrome".format(word))
 else:
     print("'{}' is not a palindrome".format(word))
+
+sentence = input("Please enter a sentence to check: ")
+if palindrome_sentence(sentence):
+    print("'{}' is a palindrome".format(sentence))
+else:
+    print("'{}' is not a palindrome".format(sentence))
