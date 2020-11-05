f60c658988f9686e288cd16a9f13239872b77b8b
Michael Miles <michael@miles-me.net>
2020-11-05 09:22:43 -0600

    Update banner.py

--- Section6/banner.py
+++ Section6/banner.py
@@ -1,1 +1,16 @@
 __author__ = "Michael E Miles"
+
+
+def banner_text(text):
+    screen_width = 80
+    if len(text) > screen_width - 4:
+        print("EEK!!")
+        print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
+
+    if text == "*":
+        print("*" * screen_width)
+    else:
+        output_string = "**{}**".format(text.center(screen_width - 4))
+        print(output_string)
+
+
