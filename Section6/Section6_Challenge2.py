e58f0447051ff4b03c9e2a76ea5f2dcf30e28368
Michael Miles <michael@miles-me.net>
2020-11-12 07:40:58 -0600

    Update Section6_Challenge2.py
    Completed Challenge 2 perfectly.

--- Section6/Section6_Challenge2.py
+++ Section6/Section6_Challenge2.py
@@ -1,11 +1,23 @@
 __author__ = "Michael E Miles"
 
 
-def factorial(number = int) -> int:
+def factorial(value: int) -> int:
     """
     Find factorial of given number.
 
-    :param number: number to find factorial
+    :param value: number to find factorial
     :return: factorial of given number
     """
-    return number
+    if value == 0:
+        return 1
+    elif value == 1 or value == 2:
+        return value
+    else:
+        new_value = 2
+        for index in range(3, value +1):
+            new_value *= index
+    return new_value
+
+
+for i in range(36):
+    print("{} {}".format(i, factorial(i)))
