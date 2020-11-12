d0f16b780f830284e5cfd36deab1a11134a97bc5
Michael Miles <michael@miles-me.net>
2020-11-12 08:30:32 -0600

    Create star_examples.py

---
+++ Section6/star_examples.py
@@ -0,0 +1,15 @@
+__author__ = "Michael E Miles"
+numbers = (0, 1, 2, 3, 4, 5)
+
+# print(numbers, sep=";")
+# print(*numbers, sep=";")
+# print(0, 1, 2, 3, 4, 5, sep=";")
+
+
+def test_star(*args):
+    print(args)
+    for x in args:
+        print(x)
+
+
+test_star(0, 1, 2, 3, 4, 5)
