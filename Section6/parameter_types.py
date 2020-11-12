ef7c83e51206756720f3cac937a94e50dbe72c68
Michael Miles <michael@miles-me.net>
2020-11-12 14:20:18 -0600

    Create parameter_types.py

---
+++ Section6/parameter_types.py
@@ -0,0 +1,20 @@
+__author__ = "Michael E Miles"
+
+
+def func(p1, p2, *args, k, **kwargs) -> None:
+    """
+    purely theoretical function.
+
+    :param p1: positional or keyword parameter
+    :param p2: positional or keyword parameter
+    :param args:  variable, positional parameter
+    :param k: keyword parameter
+    :param kwargs: variable keyword parameter
+    """
+    print("positional-or-keyword:... {}, {}".format(p1, p2))
+    print("var-positional (*args):.. {}".format(args))
+    print("keyword:................. {}".format(k))
+    print("var-keyword (**kwargs):...{}".format(kwargs))
+
+
+func(1, 2, 3, 4, 5, k=6, Mike=58, Rick=73, Tyra=68, Chris=70, Diane=56)
