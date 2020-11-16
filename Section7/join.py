4066f14bacdf384f33d219e61d18f47c089b97d6
Michael Miles <michael@miles-me.net>
2020-11-16 14:45:52 -0600

    Update join.py

--- Section7/join.py
+++ Section7/join.py
@@ -1,1 +1,10 @@
 __author__ = "Michael E Miles"
+myList = ['a', 'b', 'c', 'd',]
+
+# newString = ''
+# for c in myList:
+#     newString += c + ", "
+
+newString = ", ".join(myList)
+
+print(newString)
