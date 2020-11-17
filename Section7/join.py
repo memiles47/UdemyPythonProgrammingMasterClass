846472cec23ff6072f9b626512c0d752a8b9e6fc
Michael Miles <michael@miles-me.net>
2020-11-17 14:03:02 -0600

    Update join.py

--- Section7/join.py
+++ Section7/join.py
@@ -1,10 +1,17 @@
 __author__ = "Michael E Miles"
 myList = ['a', 'b', 'c', 'd']
+letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "123456789"
 
 # newString = ''
 # for c in myList:
 #     newString += c + ", "
 
 newString = ", ".join(myList)
+newStringAgain = ", ".join(letters)
+newNumbers = "-mississippi ".join(numbers)
 
 print(newString)
+print(newStringAgain)
+print(newNumbers)
+print(len(newNumbers))
