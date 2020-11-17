9538aa2c544f1d616e793aae7e10b8d60a104c3a
Michael Miles <michael@miles-me.net>
2020-11-17 14:40:19 -0600

    Create map.py

---
+++ Section7/map.py
@@ -0,0 +1,11 @@
+__author__ = "Michael E Miles"
+locations = {0: "You are sitting in front of a computer leaning Python",
+             1: "You are standing at the end of a road before a small "
+                "brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a small "
+                "stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in the forest",
+             }
+
