d2b38e6f3478697d7b7d1c3814bf8a56e1a513c3
Michael Miles <michael@miles-me.net>
2020-11-17 14:58:05 -0600

    Update map.py

--- Section7/map.py
+++ Section7/map.py
@@ -9,3 +9,10 @@ locations = {0: "You are sitting in front of a computer leaning Python",
              5: "You are in the forest",
              }
 
+exits = [{'Q': 0},
+         {'W': 2, 'E': 3, 'N': 5, 'S': 4, 'Q': 0},
+         {'N': 5, 'Q': 0},
+         {'W': 1, 'Q': 0},
+         {'N': 1, 'W': 2, 'Q': 0},
+         {'W': 2, 'S': 1, 'Q': 0},
+         ]
