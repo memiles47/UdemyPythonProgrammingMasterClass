d3674e5ce10f30ae03ee2293e123fc776fd5bfed
Michael Miles <michael@miles-me.net>
2020-11-19 14:01:10 -0600

    Update sets.py

--- Section7/sets.py
+++ Section7/sets.py
@@ -1,11 +1,34 @@
 __author__ = "Michael E Miles"
-farm_animals = {"sheep", "cow", "hen"}
-print(farm_animals)
+# farm_animals = {"sheep", "cow", "hen"}
+# print(farm_animals)
+#
+# for animal in farm_animals:
+#     print(animal)
+#
+# print("*" * 40)
+#
+# wild_animals = set(["lion", "tiger", "panther", "elephant", "hare"])
+# print(wild_animals)
+#
+# for animal in wild_animals:
+#     print(animal)
+#
+# farm_animals.add("horse")
+# wild_animals.add("horse")
+#
+# print(farm_animals)
+# print(wild_animals)
+#
+# empty_set = set()
+# print(empty_set)
+#
+# empty_set_2 = {}
+# empty_set.add("a")
+# empty_set_2.add("a")
 
-for animal in farm_animals:
-    print(animal)
+even = set(range(0, 40, 2))
+print(even)
 
-print("*" * 40)
-
-wild_animals = set(["lion", "tiger", "panther", "elephant", "hare"])
-print(wild_animals)
+squares_tuple = (4, 6, 9, 16, 25)
+squares = set(squares_tuple)
+print(squares)
