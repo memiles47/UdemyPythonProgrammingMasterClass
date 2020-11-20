fc7732a94812ff70e3e06c08de0fea80f6dbc68c
Michael Miles <michael@miles-me.net>
2020-11-20 09:51:55 -0600

    Input and output
    I added the functions.py file to the section and added a seperator function to input.

--- Section8/fileio.py
+++ Section8/fileio.py
@@ -1,7 +1,28 @@
 __author__ = "Michael E Miles"
-jabber = open("c:/users/Michael/Documents/sample.txt",'r')
+from functions import print_separator
+
+jabber = open("sample.txt",'r')
 
 for line in jabber:
-    print(line)
+    if "jabberwock" in line.lower():
+        print(line, end='')
 
 jabber.close()
+
+print_separator()
+
+# Using `with` automatically closes the file if that file supports
+# without the need for the .close() method.
+with open("sample.txt", 'r') as jabber:
+    for line in jabber:
+        if "JAB" in line.upper():
+            print(line, end='')
+
+print_separator()
+
+with open("sample.txt", 'r') as jabber:
+    line = jabber.readline()
+    while line:
+        print(line, end='')
+        line = jabber.readline()
+
