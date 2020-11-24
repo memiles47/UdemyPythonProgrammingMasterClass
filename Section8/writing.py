9b92147feac7a78710d32eb30416ae4ff755fdc1
Michael Miles <michael@miles-me.net>
2020-11-24 10:49:37 -0600

    Writing a tuple to a text file
    Then reading back that tuple

--- Section8/writing.py
+++ Section8/writing.py
@@ -1,4 +1,5 @@
 __author__ = "Michael E Miles"
+
 from functions import print_separator
 
 cities = ["Adelaide", "Alice Springs", "Darwin", "Melbourne", "Sydney"]
@@ -18,5 +19,15 @@ for city in cities_again:
     print(city)
 
 imelda = "More Mayhem", "Imelda May", 2011, (
-    (1, "Pulling the Rug"), (2, "Psycho"), ()
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"),
+    (4, "Kentish Town Waltz")
 )
+
+with open("imelda3.txt", 'w') as imelda_file:
+    print(imelda, file=imelda_file)
+
+with open("imelda3.txt", 'r') as imelda_file:
+    print(imelda, file=imelda_file)
+
+
+print(type(imelda))
