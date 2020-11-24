a34ed3df6acbe451d03179a62d02f460765129bf
Michael Miles <michael@miles-me.net>
2020-11-24 07:34:24 -0600

    Writing to files

--- Section8/writing.py
+++ Section8/writing.py
@@ -1,1 +1,9 @@
 __author__ = "Michael E Miles"
+from functions import print_separator
+
+cities = ["Adelaide", "Alice Springs", "Darwin", "Melbourne", "Sydney"]
+
+with open("cities.txt", 'w') as city_file:
+    for city in cities:
+        print(city, file=city_file)
+        
