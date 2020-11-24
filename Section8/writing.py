848245c43036bb97a2c1e831cbcb4f8ee50b2571
Michael Miles <michael@miles-me.net>
2020-11-24 09:03:08 -0600

    Update writing.py

--- Section8/writing.py
+++ Section8/writing.py
@@ -5,5 +5,6 @@ cities = ["Adelaide", "Alice Springs", "Darwin", "Melbourne", "Sydney"]
 
 with open("cities.txt", 'w') as city_file:
     for city in cities:
-        print(city, file=city_file)
+        print(city, file=city_file, flush=True)
+
         
