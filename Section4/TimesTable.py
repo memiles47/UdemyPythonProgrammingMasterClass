212b0297e0dddaac0083fa31e9a9b3d68d7fc561
Michael Miles <michael@miles-me.net>
2020-11-25 07:42:55 -0600

    Binary file read/write

--- Section4/TimesTable.py
+++ Section4/TimesTable.py
@@ -1,5 +1,5 @@
 __author__ = "Michael E Miles"
 for i in range(1, 13):
     for j in range(1, 13):
-        print("{} time {} is {}".format(j, i, i * j))
+        print("{} times {} is {}".format(j, i, i * j))
     print("-" * 18)
---
+++ Section8/pickling.py
@@ -0,0 +1,14 @@
+__author__ = "Michael E Miles"
+
+import pickle
+from functions import print_separator
+
+imelda = ("More Mayhem",
+          "Imelda May",
+          "2011",
+          ((1, "Pulling the Rug"),
+           (2, "Psycho"),
+           (3, "Mayhem"),
+           (4, "Kentish Town Waltz")
+           ))
+
