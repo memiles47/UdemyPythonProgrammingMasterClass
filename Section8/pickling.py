8c2ea490eaae751ba350773dd1f3cff3e76c504b
Michael Miles <michael@miles-me.net>
2020-11-26 08:58:18 -0600

    pickline

--- Section8/pickling.py
+++ Section8/pickling.py
@@ -12,11 +12,18 @@ imelda = ("More Mayhem",
            (4, "Kentish Town Waltz")
            ))
 
+even = list(range(0, 10, 2))
+odd = list(range(1, 10, 2))
+
 with open("imelda.pickle", "wb") as pickle_file:
     pickle.dump(imelda, pickle_file)
+    pickle.dump(even, pickle_file)
+    pickle.dump(odd, pickle_file)
+    pickle.dump(2998302, pickle_file)
 
 with open("imelda.pickle", "rb") as imelda_pickled:
     imelda2 = pickle.load(imelda_pickled)
+
 
 print(imelda2)
 
@@ -27,3 +34,6 @@ print(year)
 for track in track_list:
     track_number, track_title = track
     print("{}:\t{}".format(track_number, track_title))
+
+
+
