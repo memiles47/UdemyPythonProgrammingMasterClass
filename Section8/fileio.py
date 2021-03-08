46a7b12e8484539bccf17c0b897c3daf0c8e1379
Michael Miles <michael@miles-me.net>
2021-03-08 17:46:18 -0600

    Update fileio.py
    Switched to relative file location

--- Section8/fileio.py
+++ Section8/fileio.py
@@ -1,6 +1,7 @@
 __author__ = "Michael E Miles"
 #jabber = open("E:\\Repositories\\UdemyPythonProgrammingMasterClass\\Section8\\sample.txt", 'r')
-jabber = open("C:\\Users\\Michael\\OneDrive\\Desktop\\sample.txt", 'r')
+#jabber = open("C:\\Users\\Michael\\OneDrive\\Desktop\\sample.txt", 'r')
+jabber = open("sample.txt", 'r')
 
 for line in jabber:
     print(line)
