2664af9e4b2ae9910abd7f987f62a39a4dc0163e
Michael Miles <michael@miles-me.net>
2021-03-08 18:48:37 -0600

    Update fileio4.py

--- Section8/fileio4.py
+++ Section8/fileio4.py
@@ -1,7 +1,14 @@
 __author__ = "Michael E Miles"
+from functions import print_separator
+
 with open("sample.txt", 'r') as jabber:
     lines = jabber.readlines()
 # print(lines)
 
 for line in lines:
+    print(line, end='')
+
+print_separator()
+
+for line in lines[::-1]:
     print(line, end='')
